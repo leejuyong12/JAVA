819c3a9ca9d5576b8c50a2e9ab7047996373db7a
Juyong <ht9943@naver.com>
2022-11-08 22:34:09 +0900

    2884

---
+++ Algorithm/Baekjoon_2884.java
@@ -0,0 +1,23 @@
+package Algorithm;
+import java.util.Scanner;
+
+public class Baekjoon_2884 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int H = in.nextInt();
+        int M = in.nextInt();
+        in.close();
+
+        if(M<45){
+            H -= 1;
+            M = 60 - (45 - M);
+            if(H<0){
+                H = 23;
+            }
+            System.out.println(H + " " + M);
+        }else {
+            System.out.println(H + " " + (M-45));
+        }
+
+    }
+}
