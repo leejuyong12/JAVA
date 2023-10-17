f934629ce1bac470ad89c1ec32da1777407cd540
Juyong <ht9943@naver.com>
2023-10-17 23:00:27 +0900

    프로그래머스 자바 3문제

---
+++ Algorithm/TEST.java
@@ -0,0 +1,21 @@
+package Algorithm;
+
+
+import java.util.Scanner;
+public class TEST {
+    public static void main(String[] args) {
+        double a = 0.0d;
+        double b = 0.0d;
+        double c = 0.000d;
+        a = 3.0;
+        b = 3.00;
+        c = 3.000;
+        if( a== b && b == c && a== c){
+            System.out.println("YES");
+        }else{
+            System.out.println("no");
+        }
+
+
+    }
+}
